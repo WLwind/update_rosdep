a90802e373a89bcfce40eaec553cc04712ca0287
WLwind <1121391435@qq.com>
2021-06-17 03:07:01 +0800

    upload files

---
+++ rosdep_init_update.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+sudo cp 20-default.list /etc/ros/rosdep/sources.list.d/
+sudo cp {osx-homebrew.yaml,base.yaml,python.yaml,ruby.yaml,fuerte.yaml,index-v4.yaml} /etc/ros/rosdep/
+sudo cp -r {foxy,galactic,kinetic,melodic,noetic,rolling} /etc/ros/rosdep/
+sudo sed -i "s/'https:\/\/raw.githubusercontent.com\/ros\/rosdistro\/master\/index-v4.yaml'/'file:\/\/\/etc\/ros\/rosdep\/index-v4.yaml'/g" /usr/lib/python2.7/dist-packages/rosdistro/__init__.py
+rosdep update
